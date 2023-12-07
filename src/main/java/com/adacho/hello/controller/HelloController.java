53498f682d70c4634ba9be204370fa69ff85cd88
jonghyeok <jonghyeok.bae@gmail.com>
2023-12-07 13:45:41 +0900

    hello commit

--- src/main/java/com/adacho/hello/controller/HelloController.java
+++ src/main/java/com/adacho/hello/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	@GetMapping("/hello")
 	public String helloPage(Model model) {
-		model.addAttribute("greeting", "Hahaha hohoho 999999!!");
+		model.addAttribute("greeting", "Hello! Everyone!");
 		return "hello";
 	}
 }
